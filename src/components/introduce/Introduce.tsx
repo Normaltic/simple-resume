b63b3c9304178ffbb4f54709b2268eb0fd1d664a
Yunji Kim <normaltic@gmail.com>
2025-02-25 18:25:30 +0900

    Add temporal Introduce

---
+++ src/components/introduce/Introduce.tsx
@@ -0,0 +1,32 @@
+import Section from "@/components/common/Section";
+
+function Introduce() {
+  return (
+    <Section title="Introduce" className="[&>p]:indent-2 [&>p]:mb-4">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem
+        amet labore vel veritatis delectus reprehenderit nulla blanditiis neque
+        ipsum, velit atque ipsam natus tempora officia facilis qui.
+        Consequuntur, doloribus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem
+        amet labore vel veritatis delectus reprehenderit nulla blanditiis neque
+        ipsum, velit atque ipsam natus tempora officia facilis qui.
+        Consequuntur, doloribus. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Dolorum quidem amet labore vel veritatis delectus
+        reprehenderit nulla blanditiis neque ipsum, velit atque ipsam natus
+        tempora officia facilis qui. Consequuntur, doloribus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quidem
+        amet labore vel veritatis delectus reprehenderit nulla blanditiis neque
+        ipsum, velit atque ipsam natus tempora officia facilis qui.
+        Consequuntur, doloribus.
+      </p>
+      <p></p>
+    </Section>
+  );
+}
+
+export default Introduce;
