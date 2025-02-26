2331a151a5445469a454fdd92fdaa285e989d7e3
Yunji Kim <normaltic@gmail.com>
2025-02-26 13:24:35 +0900

    Fix to remove unordered list's margin

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -7,14 +7,13 @@ function Skill() {
     <Section title="Skill">
       <Header4 className="mb-2">Frontend</Header4>
       <UnorderedList
-        className="mb-6"
         items={[
           "React, Redux, Zustand, (Next.js)",
           "Styled-components, SCSS, (Tailwind.css)"
         ]}
       />
       <Header4 className="mb-2">Backend</Header4>
-      <UnorderedList className="mb-6" items={["Express.js, (MongoDB)"]} />
+      <UnorderedList items={["Express.js, (MongoDB)"]} />
       <Header4 className="mb-2">Build & Deploy</Header4>
       <UnorderedList items={["Webpack, Rollup, AWS Amplify, EC2, (esbuild)"]} />
     </Section>
