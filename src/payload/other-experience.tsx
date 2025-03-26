5fc1017ebfac62c47f8c7ecc0cc75a9b524445ea
Yunji Kim <normaltic@gmail.com>
2025-03-26 17:54:48 +0900

    Fix some payloads

--- src/payload/other-experience.tsx
+++ src/payload/other-experience.tsx
@@ -5,7 +5,7 @@ const payload: OtherExperiencePayload[] = [
     title: "react-daum-postcode",
     link: "https://www.npmjs.com/package/react-daum-postcode",
     descriptions: [
-      "주간 다운로드 수 4만+, 깃허브 스타 170+",
+      "주간 다운로드 수 3만+, 깃허브 스타 170+",
       "다음 우편번호 서비스를 React 환경에서 사용하기 쉽도록 만든 오픈소스 라이브러리",
       "이슈 대응, 유지 보수 등을 하며 라이브러리의 메인테이너로 활동 중"
     ]
