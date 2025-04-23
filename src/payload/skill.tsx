5bb0eb8e5e800a1a7563516daddb0ee6cf5134a5
Yunji Kim <normaltic@gmail.com>
2025-04-23 22:12:25 +0900

    Remove Styled-components in Skills

--- src/payload/skill.tsx
+++ src/payload/skill.tsx
@@ -3,10 +3,7 @@ import type { SkillPayload } from "@/components/skill/Skill";
 const payload: SkillPayload[] = [
   {
     category: "Frontend",
-    list: [
-      "React, Redux, Zustand, (Next.js)",
-      "Styled-components, SCSS, (Tailwind.css)"
-    ]
+    list: ["React, Redux, Zustand, (Next.js)", "SCSS, (Tailwind.css, emotion)"]
   },
   {
     category: "Backend",
